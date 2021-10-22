2e2c752705c99d79fef4c8117ea196e77e009069
agardina <agardina@student.42.fr>
2021-10-22 13:17:26 +0200

    In progress

--- script_test.sh
+++ script_test.sh
@@ -3,66 +3,84 @@
 ### Variables ###
 
 DIFF_DIR=diffs
-user_name=`whoami`
 file_name_prefix="test"
 file_name_suffix_a="_a"
 file_name_suffix_b="_b"
 diff_name_suffix="_diff"
-home_dir="/Users/$user_name"
+home_dir=$HOME
 test_dirs=("." "/dev/" $home_dir)
-
-### Preparation ###
-
-rm -rf diffs
-
-mkdir $DIFF_DIR
 
 ### Utils ###
 
-function get_test_file_name_a
+get_test_file_name_a()
 {
-	echo $file_name_prefix$1$file_name_suffix_a
+	echo $file_name_prefix$1_$2$file_name_suffix_a
 }
 
-function get_test_file_name_b
+get_test_file_name_b()
 {
-	echo $file_name_prefix$1$file_name_suffix_b
+	echo $file_name_prefix$1_$2$file_name_suffix_b
 }
 
-function get_diff_file_name
+get_diff_file_name()
 {
-	echo $file_name_prefix$1$diff_name_suffix
+	echo $file_name_prefix$1_$2$diff_name_suffix
 }
 
 ### Test functions ###
 
-function tests_on_dir
+# First arg = directory name, second arg = index of the directory in the director array
+tests_on_given_dir()
 {
-	options=("" "-a" "-t" "-tu" "-tU" "-tuU" "-tUu" "-uU" "-Uu" "-rtU" "-rtuU" "-rtUu" "-ruU" "-rUu" "-S" "-rS" "-St" "-StU" "-tS" "-tUS" "-R" "-Rt" "-RS" "-F")
+	options=("-1" "-1a" "-1t" "-1tu" "-1tU" "-1tuU" "-1tUu" "-1uU" "-1Uu" "-1rtU" "-1rtuU" "-1rtUu" "-1ruU" "-1rUu" "-1S" "-1rS" "-1St" "-1StU" "-1tS" "-1tUS" "-1R" "-1Rt" "-1RS" "-1F" "-1SF" "-1SFtUr")
+
+	local i=1
+	local nb_passed=0
+	local nb_tests=${#options[*]}
+
+	printf "Tests on %s: " $1
+
+	for opt in ${options[*]}
+	do
+		testa=`get_test_file_name_a $2 $i`
+		testb=`get_test_file_name_b $2 $i`
+		diffname=`get_diff_file_name $2 $i`
+		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testa
+		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testb
+		ls $opt $1 >> $DIFF_DIR/$testa 2> /dev/null
+		./ft_ls $opt $1 >> $DIFF_DIR/$testb 2> /dev/null
+		diff $DIFF_DIR/$testa $DIFF_DIR/$testb > $DIFF_DIR/$diffname
+		if (( $? == 0 )) ; then
+			(( nb_passed++ ))
+			mv $DIFF_DIR/$diffname $DIFF_DIR/$diffname"_OK"
+		fi
+		(( i++ ))
+	done
+
+	printf "%d/%d passed\n" $nb_passed $nb_tests
 }
 
-function tests_on_dir_long_format
+tests_on_given_dir_long_format()
 {
 	options=("-l" "-l@" "-le" "-l@e" "-ln" "-Rl" "-lF")
 }
 
 ### Tests ###
 
-function tests_on_dirs
+tests_on_dirs()
 {
-	i=0
+	local i=1
 
 	for current_dir in ${test_dirs[*]}
 	do
-		i=`expr $i + 1`
-		printf "%s:\n" $current_dir
-		testa=`get_test_file_name_a $i`
-		testb=`get_test_file_name_b $i`
-		diffname=`get_diff_file_name $i`
-		ls -r1tU $current_dir > $DIFF_DIR/$testa
-		./ft_ls -tU $current_dir > $DIFF_DIR/$testb
-		diff $DIFF_DIR/$testa $DIFF_DIR/$testb > $DIFF_DIR/$diffname
+		# printf "%s:\n" $current_dir
+		tests_on_given_dir $current_dir $i
+		(( i++ ))
 	done
 }
+
+rm -rf diffs
+
+mkdir $DIFF_DIR
 
 tests_on_dirs
