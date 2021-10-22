b835d9af360dc6d86ee70fffe25e0b12326ab6ad
agardina <agardina@student.42.fr>
2021-10-22 17:10:59 +0200

    In progress

--- script_test.sh
+++ script_test.sh
@@ -9,7 +9,7 @@ file_name_suffix_b="_b"
 diff_name_suffix="_diff"
 home_dir=$HOME
 # test_dirs=("." "/dev/" $home_dir)
-test_dirs=("." $home_dir)
+test_dirs=("." "test" "test2" $home_dir)
 
 ### Utils ###
 
@@ -33,7 +33,7 @@ get_diff_file_name()
 # First arg = directory name, second arg = index of the directory in the director array
 tests_on_given_dir()
 {
-	options=("-1" "-1a" "-1t" "-1tu" "-1tU" "-1tuU" "-1tUu" "-1uU" "-1Uu" "-1rtU" "-1rtuU" "-1rtUu" "-1ruU" "-1rUu" "-1S" "-1rS" "-1St" "-1StU" "-1tS" "-1tUS" "-1R" "-1Rt" "-1RS" "-1F" "-1SF" "-1SFtUr")
+	options=("-1" "-1a" "-1t" "-1tu" "-1tU" "-1tuU" "-1tUu" "-1uU" "-1Uu" "-1rtU" "-1rtuU" "-1rtUu" "-1ruU" "-1rUu" "-1S" "-1rS" "-1St" "-1StU" "-1tS" "-1tUS" "-1F" "-1SF" "-1SFtUr")
 
 	local i=1
 	local nb_passed=0
@@ -55,16 +55,18 @@ tests_on_given_dir()
 			(( nb_passed++ ))
 			mv $DIFF_DIR/$diffname $DIFF_DIR/$diffname"_OK"
 		fi
+		echo -n "./ft_" | cat - $DIFF_DIR/$testb > $DIFF_DIR/temp && mv $DIFF_DIR/temp $DIFF_DIR/$testb
 		(( i++ ))
 	done
 
+	# Print the result
 	printf "%d/%d passed\n" $nb_passed $nb_tests
 }
 
 # First arg = directory name, second arg = index of the directory in the director array
 tests_on_given_dir_long_printing()
 {
-	options=("-l" "-l@" "-le" "-l@e" "-ln" "-Rl" "-lF" "-la" "-lt" "-ltu" "-ltU" "-ltuU" "-ltUu" "-luU" "-lUu" "-lrtU" "-lrtuU" "-lrtUu" "-lruU" "-lrUu" "-lS" "-lrS" "-lSt" "-lStU" "-ltS" "-ltUS" "-lR" "-lRt" "-lRS" "-lF" "-lSF" "-lSFtUr")
+	options=("-l" "-l@" "-le" "-l@e" "-ln" "-lF" "-la" "-lt" "-ltu" "-ltU" "-ltuU" "-ltUu" "-luU" "-lUu" "-lrtU" "-lrtuU" "-lrtUu" "-lruU" "-lrUu" "-lS" "-lrS" "-lSt" "-lStU" "-ltS" "-ltUS" "-lF" "-lSF" "-lSFtUr")
 
 	local i=1
 	local nb_passed=0
@@ -79,15 +81,18 @@ tests_on_given_dir_long_printing()
 		diffname=`get_diff_file_name $2 $i`
 		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testa
 		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testb
-		ls $opt $1 | awk '{print $1,$2,$3,$4,$5,$9}' >> $DIFF_DIR/$testa 2> /dev/null
-		./ft_ls $opt $1 | awk '{print $1,$2,$3,$4,$5,$9}' >> $DIFF_DIR/$testb 2> /dev/null
+		ls $opt $1 2> /dev/null | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9}' >> $DIFF_DIR/$testa
+		./ft_ls $opt $1 2> /dev/null | awk '{print $1,$2,$3,$4,$5,$6,$7,$8,$9}' >> $DIFF_DIR/$testb
 		diff $DIFF_DIR/$testa $DIFF_DIR/$testb > $DIFF_DIR/$diffname
 		if (( $? == 0 )) ; then
 			(( nb_passed++ ))
 			mv $DIFF_DIR/$diffname $DIFF_DIR/$diffname"_OK"
 		fi
+		echo -n "./ft_" | cat - $DIFF_DIR/$testb > $DIFF_DIR/temp && mv $DIFF_DIR/temp $DIFF_DIR/$testb
 		(( i++ ))
 	done
+
+	# Print the result
 	printf "%d/%d passed\n" $nb_passed $nb_tests
 }
 
@@ -123,12 +128,12 @@ tests_on_dirs_long_printing()
 		(( i++ ))
 	done
 
-	printf "\n\n"
+	printf "\n"
 }
 
 rm -rf ../diffs
 
 mkdir $DIFF_DIR
 
-tests_on_dirs
+# tests_on_dirs
 tests_on_dirs_long_printing
