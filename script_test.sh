b8e97fb4050e1e052653dc1f9f0f2927984df960
agardina <agardina@student.42.fr>
2021-10-22 15:09:50 +0200

    In progress

--- script_test.sh
+++ script_test.sh
@@ -2,13 +2,14 @@
 
 ### Variables ###
 
-DIFF_DIR=diffs
+DIFF_DIR=../diffs
 file_name_prefix="test"
 file_name_suffix_a="_a"
 file_name_suffix_b="_b"
 diff_name_suffix="_diff"
 home_dir=$HOME
-test_dirs=("." "/dev/" $home_dir)
+# test_dirs=("." "/dev/" $home_dir)
+test_dirs=("." $home_dir)
 
 ### Utils ###
 
@@ -60,9 +61,34 @@ tests_on_given_dir()
 	printf "%d/%d passed\n" $nb_passed $nb_tests
 }
 
-tests_on_given_dir_long_format()
+# First arg = directory name, second arg = index of the directory in the director array
+tests_on_given_dir_long_printing()
 {
-	options=("-l" "-l@" "-le" "-l@e" "-ln" "-Rl" "-lF")
+	options=("-l" "-l@" "-le" "-l@e" "-ln" "-Rl" "-lF" "-la" "-lt" "-ltu" "-ltU" "-ltuU" "-ltUu" "-luU" "-lUu" "-lrtU" "-lrtuU" "-lrtUu" "-lruU" "-lrUu" "-lS" "-lrS" "-lSt" "-lStU" "-ltS" "-ltUS" "-lR" "-lRt" "-lRS" "-lF" "-lSF" "-lSFtUr")
+
+	local i=1
+	local nb_passed=0
+	local nb_tests=${#options[*]}
+
+	printf "Tests on %s: " $1
+
+	for opt in ${options[*]}
+	do
+		testa=`get_test_file_name_a $2 $i`
+		testb=`get_test_file_name_b $2 $i`
+		diffname=`get_diff_file_name $2 $i`
+		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testa
+		printf "ls %s %s\n\n" $opt $1 > $DIFF_DIR/$testb
+		ls $opt $1 | awk '{print $1,$2,$3,$4,$5,$9}' >> $DIFF_DIR/$testa 2> /dev/null
+		./ft_ls $opt $1 | awk '{print $1,$2,$3,$4,$5,$9}' >> $DIFF_DIR/$testb 2> /dev/null
+		diff $DIFF_DIR/$testa $DIFF_DIR/$testb > $DIFF_DIR/$diffname
+		if (( $? == 0 )) ; then
+			(( nb_passed++ ))
+			mv $DIFF_DIR/$diffname $DIFF_DIR/$diffname"_OK"
+		fi
+		(( i++ ))
+	done
+	printf "%d/%d passed\n" $nb_passed $nb_tests
 }
 
 ### Tests ###
@@ -71,16 +97,38 @@ tests_on_dirs()
 {
 	local i=1
 
+	printf "########## Test on directories ##########\n\n"
+
 	for current_dir in ${test_dirs[*]}
 	do
 		# printf "%s:\n" $current_dir
 		tests_on_given_dir $current_dir $i
 		(( i++ ))
 	done
+
+	printf "\n\n"
 }
 
-rm -rf diffs
+tests_on_dirs_long_printing()
+{
+	local i=1
+
+	printf "########## Test on directories - long printing ##########\n\n"
+
+
+	for current_dir in ${test_dirs[*]}
+	do
+		# printf "%s:\n" $current_dir
+		tests_on_given_dir_long_printing $current_dir $i
+		(( i++ ))
+	done
+
+	printf "\n\n"
+}
+
+rm -rf ../diffs
 
 mkdir $DIFF_DIR
 
 tests_on_dirs
+tests_on_dirs_long_printing
