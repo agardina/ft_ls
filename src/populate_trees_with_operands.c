47878265b2b62bdaff1a7b8b1ecb301ab1cf5a38
agardina <agardina@student.42.fr>
2021-11-01 18:43:23 +0100

    Corrected a bug related to non-existing operands

--- src/populate_trees_with_operands.c
+++ src/populate_trees_with_operands.c
@@ -66,28 +66,23 @@ static int	get_file_stat(t_ls *ls, char *name, t_ls_tree_node *content)
 **
 ** \param ls the ft_ls structure
 ** \param path the name of the operand
-**
-** \retval 0 if success or if error with the lstat function
-** \retval 1 in case of error
 */
-static int	from_operand_to_tree_node(t_ls *ls, char *name)
+static void	from_operand_to_tree_node(t_ls *ls, char *name)
 {
 	t_ls_tree_node	content;
 	int				ret;
 
 	if (get_file_stat(ls, name, &content))
-		return (1);
+		return ;
 	content.path = name;
 	content.fullpath = name;
 	ret = 0;
 	if (!is_directory(&content) || is_option_activated(ls,
 			FL_DISPLAY_DIR_AS_PLAIN_FILES))
-		ret = ft_btree_gen_add_node(&ls->main_files_tree, (void *)&content);
+		ft_btree_gen_add_node(&ls->main_files_tree, (void *)&content);
 	else
-		ret = ft_btree_gen_add_node(&ls->main_dir_tree, (void *)&content);
-	if (ret)
-		return (1);
-	return (0);
+		ft_btree_gen_add_node(&ls->main_dir_tree, (void *)&content);
+	return ;
 }
 
 /**
@@ -108,8 +103,7 @@ static int	populate_trees_with_operands_cb(t_ls *ls, t_btree_str_node *node)
 		return (1);
 	while (node->count)
 	{
-		if (from_operand_to_tree_node(ls, node->content))
-			return (1);
+		from_operand_to_tree_node(ls, node->content);
 		node->count--;
 	}
 	if (populate_trees_with_operands_cb(ls, node->right_child))
